f02b9a658dc1e0e2bdf177299eea0714a42315fc
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-05-22 14:12:38 +0200

    22.05

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ['dist/*html'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Kumbh Sans', 'sans-serif'],
+			},
+			colors: {
+				veryDarkDesaturatedBlue: 'hsl(238, 29%, 16%)',
+				veryDarkGrayishBlue: 'hsl(237, 12%, 33%)',
+				darkGrayishBlue: 'hsl(240, 6%, 50%)',
+				lightGrayishBlue: '(240, 6%, 91%)',
+				softRed: 'hsl(14, 88%, 65%)',
+				softViolet: 'hsl(273, 75%, 65%)',
+				softBlue: 'hsl(240, 73%, 65%)',
+			},
+		},
 	},
 	plugins: [],
 };
